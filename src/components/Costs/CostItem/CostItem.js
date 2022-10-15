76de3933c4e93cb0f8cdf7d2be3ba8895bdc8547
Alaska-999 <kristinagaranchuk@gmail.com>
2022-10-15 15:15:43 +0300

    added li to CostItem

--- src/components/Costs/CostItem/CostItem.js
+++ src/components/Costs/CostItem/CostItem.js
@@ -5,14 +5,15 @@ import Card from "../../UI/Card";
 const CostItem = (props) => {
 
     return (
-        <Card className='cost-item'>
-            <CostDate date={props.date}/>
-            <div className='cost-item__description'>
-              <div><h2>{props.description}</h2></div>
-                <div className='cost-item__price'>${props.amount}</div>
-            </div>
-
-        </Card>
+        <li>
+            <Card className='cost-item'>
+                <CostDate date={props.date}/>
+                <div className='cost-item__description'>
+                    <div><h2>{props.description}</h2></div>
+                    <div className='cost-item__price'>${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 };
 
